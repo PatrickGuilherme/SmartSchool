3ee99d4d4c86319411d3e7d42b2492ed0319964e
Patrick Guilherme da Silva <patrick.guilhermedasilva@gmail.com>
2021-12-06 10:27:28 -0300

    Adição de atributos aos professores, criação da função de selecionar e voltar do professor

--- src/app/professores/professores.component.ts
+++ src/app/professores/professores.component.ts
@@ -6,15 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./professores.component.css']
 })
 export class ProfessoresComponent implements OnInit {
-  public titulo = "Professores"
+  public titulo = "Professores";
+  public professorSelecionado: string;
 
   public professores =[
-    {nome: 'Soussa'},
-    {nome: 'Sergio'},
-    {nome: 'Lapa'},
-    {nome: 'Hilton'},
-    {nome: 'Hernane'},
+    {id: '1', nome: 'Soussa', disciplina: 'Banco de dados'},
+    {id: '2', nome: 'Sergio', disciplina: 'Padrões de projeto'},
+    {id: '3', nome: 'Lapa', disciplina: 'Linguagem de programação'},
+    {id: '4', nome: 'Hilton', disciplina: 'UI/UX'},
+    {id: '5', nome: 'Hernane', disciplina: 'Grafos'},
   ]
+
+  public ProfessorSelect(professor: any){
+    this.professorSelecionado = professor.nome;
+  }
+
+  public Voltar(){
+    this.professorSelecionado = '';
+  }
+
   constructor() { }
 
   ngOnInit() {
