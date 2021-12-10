f5357e787f784f38288c3c1901001e7dceb3acbd
Patrick Guilherme da Silva <patrick.guilhermedasilva@gmail.com>
2021-12-10 13:00:34 -0300

    Alunos componente

--- src/app/alunos/alunos.component.ts
+++ src/app/alunos/alunos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Aluno } from '../models/Aluno';
 
 @Component({
@@ -7,6 +8,8 @@ import { Aluno } from '../models/Aluno';
   styleUrls: ['./alunos.component.css']
 })
 export class AlunosComponent implements OnInit {
+
+  public alunoForm: FormGroup;
   public titulo = 'Alunos';
   public alunoSelecionado: Aluno;
 
@@ -18,17 +21,31 @@ export class AlunosComponent implements OnInit {
     {id: 5, nome: 'Robert', sobrenome: 'Gusmão', telefone: 38665543}
   ]
 
-  public AlunoSelect(aluno: Aluno){
-    this.alunoSelecionado = aluno;
+  constructor(private fb: FormBuilder) {
+    this.CriarForm()
   }
-  public Voltar(){
-    this.alunoSelecionado = null;
-  }
-
-  constructor() {
-   }
 
   ngOnInit(): void {
   }
 
+  public CriarForm(){
+    this.alunoForm = this.fb.group({
+      nome: ['', Validators.required],
+      sobrenome: ['', Validators.required],
+      telefone: ['', Validators.required]
+    })
+  }
+
+  public AlunoSubmit(){
+    console.log(this.alunoForm.value);
+  }
+
+  public AlunoSelect(aluno: Aluno){
+    this.alunoSelecionado = aluno;
+    this.alunoForm.patchValue(aluno);
+  }
+
+  public Voltar(){
+    this.alunoSelecionado = null;
+  }
 }
